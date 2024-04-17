fffd279fe09808efe5d2815668bc914ea7eb87ca
Monarche <petiot.jonathan@gmail.com>
2024-04-17 13:53:24 +0200

    Ajout page reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,11 @@ export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
   new Route("/carte", "La carte", "/pages/carte.html"),
-  new Route("/reservation", "Les réservations", "/pages/reservation.html"),
+  new Route(
+    "/reservation",
+    "Vos réservations",
+    "/pages/reservations/allResa.html"
+  ),
   new Route("/compte", "Mon compte", "/pages/auth/compte.html"),
   new Route("/signin", "Connexion", "/pages/auth/signin.html"),
   new Route("/signup", "Inscription", "/pages/auth/signup.html"),
