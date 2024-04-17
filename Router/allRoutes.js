5e25bc2d14ae3a5ae2b0de33382abcc09c54442a
Monarche <petiot.jonathan@gmail.com>
2024-04-17 13:05:00 +0200

    ajout des auth

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,16 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
+  new Route("/carte", "La carte", "/pages/carte.html"),
+  new Route("/reservation", "Les réservations", "/pages/reservation.html"),
+  new Route("/compte", "Mon compte", "/pages/auth/compte.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route(
+    "/editPassword",
+    "Modifier le password",
+    "/pages/auth/editPassword.html"
+  ),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
