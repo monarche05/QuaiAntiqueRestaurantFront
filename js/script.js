b889ba0a07cfdb1c0de8799d9c5609b7eab75774
Monarche <petiot.jonathan@gmail.com>
2024-04-22 15:41:09 +0200

    debug

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const tokenCookieName = "accesstoken";
 const roleCookieName = "role";
 const signoutBtn = document.getElementById("signout-btn");
-const apiUrl = "http://127.0.0.1:8000/api/";
+const apiUrl = "https://127.0.0.1:8000/api/";
 
 signoutBtn.addEventListener("click", signout);
 
